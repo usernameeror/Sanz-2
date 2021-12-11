9c06fc46f2b486b5a8d961f3e713350b239667bf
BINTANG XD <94116284+usernameeror@users.noreply.github.com>
2021-12-11 23:03:18 +0700

    Update Sanz.py

--- Sanz.py
+++ Sanz.py
@@ -163,7 +163,7 @@ def menu():
     print(" \x1b[1;92m[\x1b[1;93m04\x1b[1;92m] \x1b[1;93mCrack ID Postingan")
     print(" \x1b[1;92m[\x1b[1;93m05\x1b[1;92m] \x1b[1;93mCrack ID FB New")
     print(" \x1b[1;92m[\x1b[1;93m06\x1b[1;92m] \x1b[1;93mCrack Dari Pencarian Nama")
-    print(" \x1b[1;92m[\x1b[1;93m07\x1b[1;92m] \x1b[1;93mSettings User Agent")
+    print(" \x1b[1;92m[\x1b[1;93m07\x1b[1;92m] \x1b[1;93mSettings User Agent \x1b[1;92mU\x1b[1;97m/\x1b[1;93mA")
     print(" \x1b[1;92m[\x1b[1;93m08\x1b[1;92m] \x1b[1;93mCheck Opsi CheckPoint")
     print(" \x1b[1;92m[\x1b[1;93m09\x1b[1;92m] \x1b[1;93mCheck Hasil Crack")
     print(" \x1b[1;92m[\x1b[1;93m10\x1b[1;92m] \x1b[1;93mLaporkan Bug Script")
