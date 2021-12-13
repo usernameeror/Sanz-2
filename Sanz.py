2c96e82749ebcd81d511aec19b6b5bbf20630f28
BINTANG XD <94116284+usernameeror@users.noreply.github.com>
2021-12-13 20:51:02 +0700

    Update Sanz.py

--- Sanz.py
+++ Sanz.py
@@ -428,8 +428,7 @@ def buatngecek():
 			continue
 		except Exception as e:
 			continue
-		print("\n")
-	exit(f"{bulat} Selesai...")
+		print("\n"))
 def log_hasil(user, pasw, ttll):
     print(war+C+user+"|"+pasw+" | "+ttll)
     ua = "Mozilla/5.0 (Linux; Android 11; vivo 1904 Build/RP1A.200720.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36"
