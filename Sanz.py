339735dc930953742b5045a48cfcfa075ee2b721
BINTANG XD <94116284+usernameeror@users.noreply.github.com>
2021-12-17 08:48:54 +0700

    Update Sanz.py

--- Sanz.py
+++ Sanz.py
@@ -520,11 +520,11 @@ def cek_game(cookie):
 
 ###GANTI USER AGENT###
 def seting_yntkts():
-    print '\n %s1%s \x1b[1;93mganti user agent'%(O,N)
-    print ' %s2%s \x1b[1;93mcheck user agent'%(O,N)
-    ytbjts = raw_input('\n %s\x1b[1;93m[%s\x1b[1;92m?%s\x1b[1;92m] choose : \x1b[1;93m'%(N,O,N))
+    print '\n ╠══%s1%s \x1b[1;93mganti user agent'%(O,N)
+    print ' ╠══%s2%s \x1b[1;93mcheck user agent'%(O,N)
+    ytbjts = raw_input('\n %s\x1b[1;93m╠══[%s\x1b[1;92m?%s\x1b[1;92m] choose : \x1b[1;93m'%(N,O,N))
     if ytbjts == '':
-        print '\n %s[%s×%s] \x1b[1;93mGak boleh kosong Kentod'%(N,M,N);time.sleep(2);seting_yntkts()
+        print '\n %s╠══[%s×%s] \x1b[1;93mGak boleh kosong Kentod'%(N,M,N);time.sleep(2);seting_yntkts()
     elif ytbjts in['1','01']:
         yo_ndak_tau_ko_tanya_saia()
     elif ytbjts in['2','02']:
@@ -532,29 +532,29 @@ def seting_yntkts():
             user_agent = open('YNTKTS.txt', 'r').read()
         except IOError:
             user_agent = '%s-'%(M)
-        print '\n %s[%s+%s] \x1b[1;93mUser Agent anda : \x1b[1;93m%s%s'%(N,O,N,H,user_agent)
-        raw_input('\n  %s[ %skembali%s ]'%(N,O,N));menu()
+        print '\n %s╠══[%s+%s] \x1b[1;93mUser Agent anda : \x1b[1;93m%s%s'%(N,O,N,H,user_agent)
+        raw_input('\n  %s╠══[ %skembali%s ]'%(N,O,N));menu()
     else:
-        print '\n %s[%s×%s] \x1b[1;93minput yang bener'%(N,M,N);time.sleep(2);seting_yntkts()
+        print '\n %s╠══[%s×%s] \x1b[1;93minput yang bener'%(N,M,N);time.sleep(2);seting_yntkts()
 # User Agent baru
 def yo_ndak_tau_ko_tanya_saia():
     os.system('rm -rf YNTKTS.txt')
-    _asu_ = raw_input('\n [%s?%s] \x1b[1;93mingin menggunakan user agent hp anda [Y/t]: '%(O,N))
+    _asu_ = raw_input('\n ╠══[%s?%s] \x1b[1;93mingin menggunakan user agent hp anda [Y/t]: '%(O,N))
     if _asu_ == '':
-        print '\n %s[%s×%s] \x1b[1;93mGak boleh kosong Kentod'%(N,M,N);yo_ndak_tau_ko_tanya_saia()
+        print '\n %s╠══[%s×%s] \x1b[1;93mGak boleh kosong Kentod'%(N,M,N);yo_ndak_tau_ko_tanya_saia()
     elif _asu_ in['Y','y']:
-        jalan('\n %s •%s \x1b[1;93mMasuk Google chrome/google biasa lalu cari\n  %s•%s %sMY USER AGENT%s \x1b[1;93mlalu copy semua user agent anda...'%(O,N,O,N,H,N));time.sleep(2);os.system('')
-        _agen_ = raw_input(' [%s?%s]\x1b[1;93m masukan user agent hp anda :%s\x1b[1;93m '%(O,N,H))
+        jalan('\n %s •%s \x1b[1;93m╠══Masuk Google chrome/google biasa lalu cari\n  %s•%s %sMY USER AGENT%s \x1b[1;93mlalu copy semua user agent anda...'%(O,N,O,N,H,N));time.sleep(2);os.system('')
+        _agen_ = raw_input(' ╠══[%s?%s]\x1b[1;93m masukan user agent hp anda :%s\x1b[1;93m '%(O,N,H))
         open('YNTKTS.txt', 'w').write(_agen_);time.sleep(2)
-        jalan('\n %s[%s✓%s] \x1b[1;92mberhasil menggunakan user agent hp anda...'%(N,H,N))
-        raw_input('\n  %s[ %skembali%s ]'%(N,O,N));menu()
+        jalan('\n %s╠══[%s✓%s] \x1b[1;92mberhasil menggunakan user agent hp anda...'%(N,H,N))
+        raw_input('\n  %s╠══[ %skembali%s ]'%(N,O,N));menu()
     elif _asu_ in['T','t']:
-        _agen_ = raw_input(' [%s?%s] \x1b[1;93mmasukan user agent :%s \x1b[1;93m'%(O,N,H))
+        _agen_ = raw_input(' ╠══[%s?%s] \x1b[1;93mmasukan user agent :%s \x1b[1;93m'%(O,N,H))
         open('YNTKTS.txt', 'w').write(_agen_);time.sleep(2)
-        jalan('\n %s[%s✓%s]\x1b[1;93m berhasil mengganti user agent...'%(N,H,N))
-        raw_input('\n  %s[ %skembali%s ]'%(N,O,N));menu()
+        jalan('\n %s╠══[%s✓%s]\x1b[1;93m berhasil mengganti user agent...'%(N,H,N))
+        raw_input('\n  %s╠══[ %skembali%s ]'%(N,O,N));menu()
     else:
-        print '\n %s[%s!%s]\x1b[1;93m [Y/t] ngentod'%(N,M,N);yo_ndak_tau_ko_tanya_saia()
+        print '\n %s╠══[%s!%s]\x1b[1;93m [Y/t] ngentod'%(N,M,N);yo_ndak_tau_ko_tanya_saia()
 
 ####LAPORAN BUG####
 def laporbug():
